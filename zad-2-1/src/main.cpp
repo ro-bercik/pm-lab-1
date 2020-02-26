ff7946f690e89a8c42bca091c46665b98ed785f3
Imię Nazwisko <imie.nazwisko@student@wat.edu.pl>
2020-02-26 13:47:32 +0100

    Zadanie 2-2-3

--- zad-2-1/src/main.cpp
+++ zad-2-1/src/main.cpp
@@ -1,18 +1,44 @@
 #include <Arduino.h>
+#include <morse.h>
 
 #define LED_RED 10
 
 void setup()
 {
-  // put your setup code here, to run once:
   pinMode(LED_RED, OUTPUT);
 }
 
+
+
 void loop()
 {
-  // put your main code here, to run repeatedly:
-  digitalWrite(LED_RED, HIGH);
-  delay(500);
-  digitalWrite(LED_RED, LOW);
-  delay(500);
+  uint8_t i;
+  for (i = 0; i < 3; i++) // Wysłanie znaku 'S'
+  {
+    digitalWrite(LED_RED, HIGH);
+    delay(M_KROPKA_T);
+    digitalWrite(LED_RED, LOW);
+    delay(M_PAUZA_T);
+  }
+  // Wysłanie znaku '0'
+  i = 0;
+  do
+  {
+    digitalWrite(LED_RED, HIGH);
+    delay(M_KRESKA_T);
+    digitalWrite(LED_RED, LOW);
+    delay(M_PAUZA_T);
+    i++;
+  } while (i < 3);
+  // Wysłanie znaku 'S'
+  i = 0;
+  while (i < 3)
+  {
+    digitalWrite(LED_RED, HIGH);
+    delay(M_KROPKA_T);
+    digitalWrite(LED_RED, LOW);
+    delay(M_PAUZA_T);
+    i++;
+  }
+  delay(M_SPACJA_T);
 }
