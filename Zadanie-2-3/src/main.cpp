45cfdcf803f0d89061ee92b3deca38d4c748fb2b
Bercik <ro_beri@gazeta.pl>
2020-04-03 17:36:41 +0200

    Wykonano zadanie 2.2.1a

--- Zadanie-2-3/src/main.cpp
+++ Zadanie-2-3/src/main.cpp
@@ -9,16 +9,20 @@ unsigned long changeTime;
 void changeLED();
 
 void setup() {
-  for (int i = 0; i < 10; i++)  {
-    pinMode(ledPin[i], OUTPUT);
-  }
+  // for (int i = 0; i < 10; i++)  {
+  //   pinMode(ledPin[i], OUTPUT);
+  // }
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
 }
 
 void loop() {
-  if ((millis() - changeTime) > ledDelay) {
-    changeLED();
-    changeTime = millis();
-  }
+  // if ((millis() - changeTime) > ledDelay) {
+  //   changeLED();
+  //   changeTime = millis();
+  // }
+  digitalWrite(LED_BUILTIN, HIGH);
+  digitalWrite(LED_BUILTIN, LOW);
 }
 
 void changeLED() {
